0923dbe899c8834ae2d6a33c78f7d5376e83f6db
Martin Kocich <kocichmartin@gmail.com>
2025-09-02 10:13:01 +0200

    FEAT: NTFS support added

--- configuration.nix
+++ configuration.nix
@@ -20,7 +20,7 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
   
-  boot.supportedFilesystems = [ "zfs" ];
+  boot.supportedFilesystems = [ "zfs" "ntfs" ];
   boot.initrd.systemd = {
     enable = false;
     services.initrd-rollback-root = {
